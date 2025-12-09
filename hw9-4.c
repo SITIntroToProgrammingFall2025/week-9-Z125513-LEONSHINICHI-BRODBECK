babdb72e572ec484a58ce47aa3041844676053a9
Z125513-LEONSHINICHI-BRODBECK <Z125513@shibaura-it.ac.jp>
2025-12-09 23:21:35 +0900

    Update hw9-4.c

--- hw9-4.c
+++ hw9-4.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int r, g, b;
+    
+    // Read the RGB values
+    scanf("%d %d %d", &r, &g, &b);
+    
+    // Convert to hexadecimal and print
+    printf("The hex code is #%02X%02X%02X\n", r, g, b);
+    
+    return 0;
+}
 
