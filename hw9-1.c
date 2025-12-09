740a5627f85de500d79d641231e35173afd9bff7
Z125513-LEONSHINICHI-BRODBECK <Z125513@shibaura-it.ac.jp>
2025-12-09 23:14:19 +0900

    Update hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // Read a 2x2 matrix
+    int a, b, c, d;
+    
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    
+    printf("You entered\n");
+    printf("%d %d\n", a, b);
+    printf("%d %d\n", c, d);
+    
+    float determinant = (a * d) - (b * c);
+    
+    printf("Determinant is %.6f\n", determinant);
+    
+    return 0;
+}
 
